5bc269fc90fc409e16dfb0143f5b2ac869fdb051
Matt <matt.nelsenn@gmail.com>
2019-12-17 17:41:47 -0600

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.create(name: "matt", email: "matt@email.com")
+User.create(name: "miranda", email: "miranda@email.com")
 
 Neighborhood.create(name: "Logan Square")
 Neighborhood.create(name: "Wicker Park")
