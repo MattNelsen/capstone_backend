c5f7544d59880bbb757d59db75858473fdf36fe1
Matt <matt.nelsenn@gmail.com>
2020-01-21 20:39:59 -0600

    removed reviews

--- app/controllers/api/reviews_controller.rb
+++
@@ -1,7 +0,0 @@
-# class Api::ReviewsController < ApplicationController
-#   def index
-#     response = HTTP.get("https://api.yelp.com/v3/businesses/1FrPC9qqfE0CdHhh6F5hZA/reviews")
-#     reviews = response.parse["reviews"]
-#     render json: { result: reviews }
-#   end
-# end
